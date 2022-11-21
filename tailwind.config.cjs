972c5940fc2b19b82252f9e0f1d6b1eea548c750
Haume <haume341@outlook.com>
2022-11-21 16:43:35 +0300

    ✨Released

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{svelte,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Figtree: 'Figtree',
+      },
+    },
+  },
+  plugins: [],
+}
